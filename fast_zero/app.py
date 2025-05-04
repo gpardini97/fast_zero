f00ea74d761408c12eec1d114d08c825de9d68e1
Gabriel Pardini <gpardini47@gmail.com>
2025-05-04 20:24:01 -0300

    feat: incremented schema validation script & html exercise endpoint

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,27 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from fast_zero.schema import Message
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/", status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
-    return {"message": "Olá Mundo!"}
+    return {'message': 'Olá Mundo!'}
+
+
+@app.get("/exercicio-html", response_class=HTMLResponse)
+def exercicio_aula_02():
+    return """
+    <html>
+      <head>
+        <title>Nosso olá mundo!</title>
+      </head>
+      <body>
+        <h1> Olá Mundo </h1>
+      </body>
+    </html>
+    """
